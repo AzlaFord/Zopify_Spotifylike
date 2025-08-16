44d04009920c818f90df1010550996b663bc5420
Bivol <vbivol394@gmail.com>
2025-08-16 18:59:22 +0300

    am facut loigca connectare mongodb

---
+++ app/lib/connect.js
@@ -0,0 +1,21 @@
+const { MongoClient } = require("mongodb");
+
+const client = new MongoClient(process.env.URI);
+run()
+
+if (!process.env.MONGODB_URI) {
+  throw new Error("Please add your Mongo URI to .env.local");
+}
+
+if (process.env.NODE_ENV === "development") {
+  if (!global._mongoClientPromise) {
+    client = new MongoClient(uri, options);
+    global._mongoClientPromise = client.connect();
+  }
+  clientPromise = global._mongoClientPromise;
+} else {
+  client = new MongoClient(uri, options);
+  clientPromise = client.connect();
+}
+
+export default clientPromise;
