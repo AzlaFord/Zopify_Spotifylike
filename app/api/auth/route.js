8da1404ff693a48e96383649b210f33493dd5426
Bivol <vbivol394@gmail.com>
2025-08-17 17:47:50 +0300

    am facut logica logare

---
+++ app/api/auth/route.js
@@ -0,0 +1,64 @@
+import createUser from "@/app/lib/auth";
+
+async function POST(request) {
+    const data = await request.json()
+    const {nume, prenume, email, dataNastere, parola} = data
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    try{
+        if(!nume){
+            return new Response(JSON.stringify({message:"nu e completat capul cu numele"}),{
+                status:400,
+                headers:{"Content-Type":"application/json"}
+            })
+        }
+        if(!prenume){
+            return new Response(JSON.stringify({message:"nu e completat capul cu prenumele"}),{
+                status:400,
+                headers:{"Content-Type":"application/json"}
+            })
+        }
+        if(!email){
+            return new Response(JSON.stringify({message:"nu e completat capul cu email"}),{
+                status:400,
+                headers:{"Content-Type":"application/json"}
+            })
+        }
+        if (!emailRegex.test(email)) {
+            return new Response(
+                JSON.stringify({ message: "Email invalid" }),
+                { status: 400, headers: { "Content-Type": "application/json" } }
+            );
+        }
+
+        if(!dataNastere){
+            return new Response(JSON.stringify({message:"nu e completat capul cu dataNastere"}),{
+                status:400,
+                headers:{"Content-Type":"application/json"}
+            })
+        }
+        
+        if(!parola){
+            return new Response(JSON.stringify({message:"nu e completat capul cu parola"}),{
+                status:400,
+                headers:{"Content-Type":"application/json"}
+            })
+        }
+
+        if (parola.length < 8) {
+            return new Response(
+                JSON.stringify({ message: "Parola trebe să aiba minim 8 caractere" }),
+                { status: 400, headers: { "Content-Type": "application/json" } }
+            );
+        }
+        const result = await createUser(data)
+        return new Response(JSON.stringify({message:"a mers totul bine user creat",result}),{
+            status:200,
+            headers:{"Content-Type":"application/json"}
+        })
+    }catch(err){
+        return new Response(JSON.stringify({message: err.message || String(err)}),{
+            status:500,
+            headers:{"Content-Type":"application/json"}
+        })
+    }
+}
