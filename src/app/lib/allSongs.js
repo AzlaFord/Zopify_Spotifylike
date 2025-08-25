de105b7386656c8ccd913aab5ae5256755bacbd8
Bivol <vbivol394@gmail.com>
2025-08-25 12:25:41 +0300

    facut chesti

--- src/app/lib/allSongs.js
+++ src/app/lib/allSongs.js
@@ -3,20 +3,31 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseAdmin = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL,
   process.env.SUPABASE_SERVICE_ROLE_KEY
-)
+);
 
-export default async function countSongs(){
-    try{
-        const {count:songCount , error:SongError} = await supabaseAdmin.from("Songs").select("*",{count:"exact",head:true})
-        if (SongError) throw SongError;
+export default async function countSongs() {
+  try {
 
-        const {data:artists , error:artistError} = await supabaseAdmin.from("Songs").select("artist",{distinct: true})
-        if (artistError) throw artistError;
-        
-        const artistCount = artists.length;
-        
-        return {success:true,message:"totul a mers bine",songs:songCount,artists:artistCount}
-    }catch(err){
-        return {success:false,message:err.message}
-    }
+    const { count: songCount, error: SongError } = await supabaseAdmin
+      .from("Songs")
+      .select("*", { count: "exact", head: true });
+    if (SongError) throw SongError;
+
+    const { data, error: artistError } = await supabaseAdmin
+      .from("Songs")
+      .select("artist"); 
+    if (artistError) throw artistError;
+
+    const uniqueArtists = [...new Set(data.map(a => a.artist))];
+    const artistCount = uniqueArtists.length;
+
+    return {
+      success: true,
+      message: "totul a mers bine",
+      songs: songCount,
+      artists: artistCount
+    };
+  } catch (err) {
+    return { success: false, message: err.message };
+  }
 }
